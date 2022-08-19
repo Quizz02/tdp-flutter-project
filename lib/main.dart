26ec53c9a21a57d8a82038c7cf58126a7e61a4f4
Juan de Dios Quiroz <quirozrodriguezj@gmail.com>
2022-08-19 02:06:20 -0500

    Feat: Added NavBar

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:tdp_flutter_project/navbar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavBar(),
+      appBar: AppBar(
+        title: Text('Inicio')
+      ),
+      body: Center(),
+    );
+  }
+}
+
+
---
+++ lib/navbar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(accountName: Text('Matías Beteta'), 
+            accountEmail: Text('amir_9_2011@hotmail.com'), 
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: ClipOval(
+                child: Image.network('https://icones.pro/wp-content/uploads/2021/02/icone-utilisateur-gris.png',
+                color: Colors.grey,
+                fit: BoxFit.cover),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.house),
+            title: Text('Inicio'),
+            onTap: () => print('Inicio!!'),
+          ),
+          ListTile(
+            leading: Icon(Icons.insert_chart_rounded),
+            title: Text('Resumen Semanal'),
+            onTap: () => print('Resumen'),
+          ),
+          ListTile(
+            leading: Icon(Icons.groups),
+            title: Text('Comunidad'),
+            onTap: () => print('Comunidad'),
+          ),
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text('Mis Reportes'),
+            onTap: () => print('Reportes'),
+          ),
+          ListTile(
+            leading: Icon(Icons.notification_important),
+            title: Text('Probabilidad'),
+            onTap: () => print('Probabilidad'),
+          ),
+          ListTile(
+            leading: Icon(Icons.warning),
+            title: Text('Reportar Incidente'),
+            onTap: () => print('Reporte de incidente'),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Configuración'),
+            onTap: () => print('Configuración'),
+          ),
+        ],
+      ),
+    );
+  }
+}
