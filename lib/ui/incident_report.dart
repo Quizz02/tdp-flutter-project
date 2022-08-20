5409e3e0579288f7daad4626a4e4d3830374c60b
Juan de Dios Quiroz <quirozrodriguezj@gmail.com>
2022-08-20 00:57:21 -0500

    feat: Add incident_report form

--- lib/ui/incident_report.dart
+++ lib/ui/incident_report.dart
@@ -14,7 +14,60 @@ class _IncidentReportState extends State<IncidentReport> {
       appBar: AppBar(
         title: Text('Reportar Incidente')
       ),
-      body: Container(),
+      body: _buildForm(),
     );
   }
+
+  Widget _buildForm() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget> [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Categoría'
+                ),
+              ),
+              Divider(
+                color: Colors.white24,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Descripción (opcional)'
+                ),
+              ),
+              Divider(
+                color: Colors.white24,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Foto (opcional)'
+                ),
+              ),
+              Divider(
+                color: Colors.white24,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Ubicación del suceso'
+                ),
+              ),
+              Divider(
+                color: Colors.white24,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Referencia del lugar'
+                ),
+              )
+            ]
+          )
+        )
+      )
+    );
+  }
+
+
 }
